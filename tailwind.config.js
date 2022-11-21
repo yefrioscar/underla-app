5b57fe88605046a04354d341797c309e9ecc0ef0
Yefri <yefrioscar9814@gmail.com>
2022-11-20 19:18:30 -0500

    first commit - home page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        default: '10px'
+      }
+    },
+  },
+  plugins: [],
+}
